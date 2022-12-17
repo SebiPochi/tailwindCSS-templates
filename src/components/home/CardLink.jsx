eeac1a546bd27d3bf61027d691725948f4a5c65b
SebiPochi <sebipochi@gmail.com>
2022-12-17 06:27:29 -0300

    fit dark mode

--- src/components/home/CardLink.jsx
+++ src/components/home/CardLink.jsx
@@ -7,7 +7,7 @@ const CardLink = ({ to }) => {
     <Link to={`/${to}`}>
         <div className='border-4 relative border-slate-800 bg-slate-300 dark:border-slate-400 dark:bg-slate-700 rounded-2xl w-full h-64'>
             <img className='absolute opacity-40 object-none rounded-xl w-full h-[15.5rem]' src={require('../../img/pinterest.png')} />
-            <BsPinterest className='relative text-5xl ml-3 mt-3'/>
+            <BsPinterest className='text-slate-800 dark:text-slate-400 relative text-5xl ml-3 mt-3'/>
         </div>
     </Link>
   )
